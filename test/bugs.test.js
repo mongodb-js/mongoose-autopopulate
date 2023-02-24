1c501c19825348e1ec4df2d428dfb7290a91c16b
Valeri Karpov <val@karpov.io>
2023-02-24 14:20:59 -0500

    fix tests

--- test/bugs.test.js
+++ test/bugs.test.js
@@ -105,13 +105,13 @@ describe('bug fixes', function() {
     const Offer = db.model('gh45_NewYork', offerSchema);
     const Mapping = db.model('gh45_Mapping', mappingSchema);
 
-      const offer = await Offer.create({ name: 'Labor Day Sale' });
-      await Mapping.create({
-        city: 'gh45_NewYork',
-        offer: offer._id
-      });
+    const offer = await Offer.create({ name: 'Labor Day Sale' });
+    await Mapping.create({
+      city: 'gh45_NewYork',
+      offer: offer._id
+    });
 
-      await Mapping.findOne();
+    await Mapping.findOne();
 
   });
 
@@ -125,20 +125,20 @@ describe('bug fixes', function() {
     const Group = db.model('gh53_Group', schema);
 
     await Person.deleteMany({});
-      await Group.deleteMany({});
+    await Group.deleteMany({});
 
-      const luke = await Person.create({ name: 'Luke Skywalker' });
-      const obiwan = await Person.create({ name: 'Obi Wan Kenobi' });
-      await Group.create({ name: 'Jedi Order', people: [luke._id] });
+    const luke = await Person.create({ name: 'Luke Skywalker' });
+    const obiwan = await Person.create({ name: 'Obi Wan Kenobi' });
+    await Group.create({ name: 'Jedi Order', people: [luke._id] });
 
-      const doc = await Group.findOne().populate('people');
-      assert.equal(doc.people[0].name, 'Luke Skywalker');
+    const doc = await Group.findOne().populate('people');
+    assert.equal(doc.people[0].name, 'Luke Skywalker');
 
-      doc.people.push(obiwan._id);
-      const res = await doc.save();
+    doc.people.push(obiwan._id);
+    const res = await doc.save();
 
-      assert.equal(res.people[0].name, 'Luke Skywalker');
-      assert.equal(res.people[1].name, 'Obi Wan Kenobi');
+    assert.equal(res.people[0].name, 'Luke Skywalker');
+    assert.equal(res.people[1].name, 'Obi Wan Kenobi');
   });
 
   it('skips post save populate if unnecessary (gh-53)', async function() {
@@ -151,19 +151,19 @@ describe('bug fixes', function() {
     const Group = db.model('gh53_Group_2', schema);
 
     await Person.deleteMany({});
-      await Group.deleteMany({});
+    await Group.deleteMany({});
 
-      const obiwan = await Person.create({ name: 'Obi Wan Kenobi' });
-      await Group.create({ name: 'Jedi Order', people: [obiwan._id] });
+    const obiwan = await Person.create({ name: 'Obi Wan Kenobi' });
+    await Group.create({ name: 'Jedi Order', people: [obiwan._id] });
 
-      const doc = await Group.findOne().populate('people');
-      assert.equal(doc.people[0].name, 'Obi Wan Kenobi');
+    const doc = await Group.findOne().populate('people');
+    assert.equal(doc.people[0].name, 'Obi Wan Kenobi');
 
-      await Person.updateOne({ name: 'Obi Wan Kenobi' }, { name: 'Ben Kenobi' });
+    await Person.updateOne({ name: 'Obi Wan Kenobi' }, { name: 'Ben Kenobi' });
 
-      const res = await doc.save();
+    const res = await doc.save();
 
-      assert.equal(res.people[0].name, 'Obi Wan Kenobi');
+    assert.equal(res.people[0].name, 'Obi Wan Kenobi');
   });
 
   it('autopopulates discriminators post find (gh-26)', async function() {
@@ -179,16 +179,16 @@ describe('bug fixes', function() {
     const Child = Base.discriminator('gh26_Child', childSchema);
     const ChildData = db.model('gh26', Schema({ name: String }));
 
-      const c = await ChildData.create({ name: 'test' });
-      await Child.create({ field: 'foo', items: [c._id] });
+    const c = await ChildData.create({ name: 'test' });
+    await Child.create({ field: 'foo', items: [c._id] });
 
-      const doc = await Base.findOne();
-      assert.ok(doc instanceof Child);
-      assert.equal(doc.items[0].name, 'test');
+    const doc = await Base.findOne();
+    assert.ok(doc instanceof Child);
+    assert.equal(doc.items[0].name, 'test');
 
-      const docs = await Base.find();
-      assert.ok(docs[0] instanceof Child);
-      assert.equal(docs[0].items[0].name, 'test');
+    const docs = await Base.find();
+    assert.ok(docs[0] instanceof Child);
+    assert.equal(docs[0].items[0].name, 'test');
   });
 
   it('handles autopopulate in nested doc array when top-level array is empty (gh-70)', async function() {
@@ -209,14 +209,14 @@ describe('bug fixes', function() {
     GameSchema.plugin(autopopulate);
     const Game = db.model('Game', GameSchema);
 
-      const player = await User.create({ name: 'test' });
-      await Game.create({ players: [], state: [] });
+    const player = await User.create({ name: 'test' });
+    await Game.create({ players: [], state: [] });
 
-      const doc = await Game.findOne();
-      doc.players.push(player._id);
-      await doc.save();
+    const doc = await Game.findOne();
+    doc.players.push(player._id);
+    await doc.save();
 
-      assert.deepEqual(doc.toObject().state, []);
+    assert.deepEqual(doc.toObject().state, []);
 
   });
 
@@ -237,18 +237,18 @@ describe('bug fixes', function() {
     ParentSchema.plugin(autopopulate);
     const ParentModel = db.model('Parent', ParentSchema);
 
-      const populated = new PopulatedModel({ name: 'my test' });
-      await populated.save();
+    const populated = new PopulatedModel({ name: 'my test' });
+    await populated.save();
 
-      const doc = new ParentModel();
-      doc.entries.push({ model: populated._id });
-      await doc.save();
+    const doc = new ParentModel();
+    doc.entries.push({ model: populated._id });
+    await doc.save();
 
-      doc.entries.push({ model: populated._id });
-      await doc.save();
+    doc.entries.push({ model: populated._id });
+    await doc.save();
 
-      assert.equal(doc.entries[0].model.name, 'my test');
-      assert.equal(doc.entries[1].model.name, 'my test');
+    assert.equal(doc.entries[0].model.name, 'my test');
+    assert.equal(doc.entries[1].model.name, 'my test');
 
   });
 
@@ -273,18 +273,18 @@ describe('bug fixes', function() {
 
     const Map = db.model('Map', mapSchema);
 
-      const e = await Enemy.create({
-        name: 'Bowser',
-        level: 10
-      });
+    const e = await Enemy.create({
+      name: 'Bowser',
+      level: 10
+    });
 
-      let map = await Map.create({
-        tiles: [[{ kind: 'Enemy', enemy: e._id }, { kind: 'Wall', color: 'Blue' }]]
-      });
+    let map = await Map.create({
+      tiles: [[{ kind: 'Enemy', enemy: e._id }, { kind: 'Wall', color: 'Blue' }]]
+    });
 
-      map = await Map.findById(map);
-      assert.equal(map.tiles[0][0].enemy.name, 'Bowser');
-      assert.equal(map.tiles[0][1].color, 'Blue');
+    map = await Map.findById(map);
+    assert.equal(map.tiles[0][0].enemy.name, 'Bowser');
+    assert.equal(map.tiles[0][1].color, 'Blue');
 
   });
 
